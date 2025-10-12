a199f710f7724ea606af2e355e086622bf56bf37
Anshul <anshulsharma1219@gmail.com>
2025-10-13 00:08:45 +0530

    javascript

--- conversionOrOperation.js
+++ conversionOrOperation.js
@@ -1,22 +1,46 @@
-let score="33"
-console.log(typeof score);
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// let score="33"
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
 
-let score1="33abc"
-console.log(typeof score1);
-let valueInNumber1 = Number(score1);
-console.log(typeof valueInNumber1);
-console.log(valueInNumber1)
+// let score1="33abc"
+// console.log(typeof score1);
+// let valueInNumber1 = Number(score1);
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1)
 
-let score2=null
-console.log(typeof score2);
-let valueInNumber2 = Number(score2);
-console.log(typeof valueInNumber2);
-console.log(valueInNumber2)
+// let score2=null
+// console.log(typeof score2);
+// let valueInNumber2 = Number(score2);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2)
 
-let score3=undefined
-console.log(typeof score3);
-let valueInNumber3 = Number(score3);
-console.log(typeof valueInNumber3);
-console.log(valueInNumber3)
+// let score3=undefined
+// console.log(typeof score3);
+// let valueInNumber3 = Number(score3);
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3)
+
+
+// ********************Operations**************
+
+let value=3
+let nagvalue = -value
+// console.log(nagvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "Anshul"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
