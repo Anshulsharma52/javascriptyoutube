ab0cbf96be562b2aa361dff3df4dffd216935143
Anshul <anshulsharma1219@gmail.com>
2025-10-13 09:28:23 +0530

    javascript

--- datatypes.js
+++ datatypes.js
@@ -18,6 +18,33 @@ let isLoggedIn = false
 // null ek object datatype h
 
 
-console.log(typeof "Anshul");  //string
-console.log(typeof undefined); //undefined
-console.log(typeof null); //object
+// console.log(typeof "Anshul");  //string
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); //object
+
+
+// premitive
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const id = Symbol('1234')
+// const anotherId = Symbol('1234')
+
+// console.log(id === anotherId);
+
+
+// Reference(Non premitive)
+// Array, Objects, Functions
+
+//Array
+const heros = ["shaktiman","naagraj","doga"];
+//Objects
+let myobj = {
+    name:"Anshul",
+    age: 20,
+}
+//function
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
